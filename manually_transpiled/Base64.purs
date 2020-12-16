b11957bbbc0bd3671a7487e982be1acb7c42e6db
radrow <radrowicki@gmail.com>
2020-12-16 12:41:08 +0100

    b64

--- manually_transpiled/Base64.purs
+++ manually_transpiled/Base64.purs
@@ -8,6 +8,7 @@ import Erlang.Builtins
 import Erlang.Binary as BIN
 import Erlang.Helpers as H
 import Data.Maybe as DM
+import Data.Either as DE
 import Prelude
 import Partial.Unsafe
 import Node.Buffer
@@ -22,14 +23,14 @@ erlps__decode__1 [ErlangBinary buf] =
   erlps__decode__1 [BIN.to_erlang_list buf]
 erlps__decode__1 [el] | DM.Just str <- H.erlangListToString el
   = case decode str of
-      Left _ -> BIN.badarg unit
-      Right u -> ErlangBinary $ u8ToBuf u
+      DE.Left _ -> EXC.badarg unit
+      DE.Right u -> ErlangBinary $ u8ToBuf u
 erlps__decode__1 [_] = EXC.badarg unit
 erlps__decode__1 args = EXC.badarity (ErlangFun 1 erlps__decode__1) args
 
 erlps__decode_to_string__1 :: ErlangFun
 erlps__decode_to_string__1 [a] =
-  erlang__binary_to_list [erlps__decode__1 [a]]
+  erlang__binary_to_list__1 [erlps__decode__1 [a]]
 erlps__decode_to_string__1 args = EXC.badarity (ErlangFun 1 erlps__decode_to_string__1) args
 
 erlps__encode__1 :: ErlangFun
@@ -39,7 +40,7 @@ erlps__encode__1 args = EXC.badarity (ErlangFun 1 erlps__encode__1) args
 
 erlps__encode_to_string__1 :: ErlangFun
 erlps__encode_to_string__1 [ErlangBinary buf] =
-  make_string $ encode $ bufToU8 buf
+  H.make_string $ encode $ bufToU8 buf
 erlps__encode_to_string__1 [el] =
-  erlps__encode_to_string__1 [erlang__list_to_binary__1]
+  erlps__encode_to_string__1 [erlang__list_to_binary__1 [el]]
 erlps__encode_to_string__1 args = EXC.badarity (ErlangFun 1 erlps__encode_to_string__1) args
