3f4d0ca53d7155d11b2a2c754d0312cbd473319b
radrow <radrowicki@gmail.com>
2020-12-17 12:10:51 +0100

    fix base64 u8ToBuf

--- manually_transpiled/Base64.js
+++ manually_transpiled/Base64.js
@@ -1,4 +1,10 @@
 "use static";
 
 exports.bufToU8 = function(buf) { return new Uint8Array(buf); };
-exports.u8ToBuf = function(u8) { return u8.buffer; };
+exports.u8ToBuf = function(u8) {
+    var buf = Buffer.alloc(u8.byteLength);
+    for (var i = 0; i < buf.length; ++i) {
+        buf[i] = u8[i];
+    }
+    return buf;
+};
