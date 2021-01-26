ac3fce8360a9ca73fa11a8c8a56066e27950fd8b
radrow <radrowicki@gmail.com>
2021-01-26 18:56:06 +0100

    Fix crypto4

--- manually_transpiled/Crypto.purs
+++ manually_transpiled/Crypto.purs
@@ -6,20 +6,14 @@ import Erlang.Type
 import Erlang.Binary as BIN
 import Erlang.Builtins as BIF
 import Erlang.Helpers as H
-import Data.Maybe as DM
 import Prelude
-import Partial.Unsafe
 import Node.Buffer
-import Unsafe.Coerce
 
 foreign import sha256Impl :: Buffer -> Buffer
 
 erlps__hash__2 :: ErlangFun
 erlps__hash__2 [ErlangAtom "sha256", ErlangBinary buf]
-  = unsafePartial
-    $ ErlangBinary
-    $ sha256Impl
-    $ buf
+  = ErlangBinary $ sha256Impl buf
 erlps__hash__2 [alg, iolist] =
   erlps__hash__2 [alg, BIF.erlang__iolist_to_binary__1 [iolist]]
 erlps__hash__2 _ = EXC.badarg unit
--- src/Crypto.js
+++ src/Crypto.js
@@ -1,7 +1,5 @@
 "use static";
 
-var SHA256 = require("crypto-js/sha256");
-
 exports.sha256Impl = function(buf) {
     return Buffer.from(crypto.createHash('sha256').update(buf).digest('hex'), 'hex');
 }
