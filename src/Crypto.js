0dc81754f244a0420a8ed5aa6fa7c0cbbab23e45
radrow <radrowicki@gmail.com>
2021-01-26 18:49:48 +0100

    Fix crypto3

--- src/Crypto.js
+++ src/Crypto.js
@@ -3,5 +3,5 @@
 var SHA256 = require("crypto-js/sha256");
 
 exports.sha256Impl = function(buf) {
-    return Buffer.from(SHA256(buf).toString(), 'hex');
+    return Buffer.from(crypto.createHash('sha256').update(buf).digest('hex'), 'hex');
 }
