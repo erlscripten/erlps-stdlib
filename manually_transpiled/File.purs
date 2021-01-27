e392e4a0c4291615f6e2f047c18c2ba59cf3dacc
radrow <radrowicki@gmail.com>
2021-01-27 12:02:26 +0100

    format_error

--- manually_transpiled/File.purs
+++ manually_transpiled/File.purs
@@ -1,12 +1,14 @@
 module File where
 
 import Erlang.Type
-import Erlang.Exception as EXC
-import Erlang.Helpers as H
-import Erlang.Binary as BIN
-import Data.Maybe as DM
 import Node.Buffer
 import Prelude
+
+import Data.Maybe as DM
+import Erlang.Binary as BIN
+import Erlang.Builtins as BIF
+import Erlang.Exception as EXC
+import Erlang.Helpers as H
 
 foreign import readFileImpl :: (Buffer -> ErlangTerm) -> ErlangTerm -> String -> ErlangTerm
 
@@ -25,3 +27,16 @@ erlps__native_name_encoding__0 args = EXC.badarity (ErlangFun 0 erlps__native_na
 erlps__get_cwd__0 :: ErlangFun
 erlps__get_cwd__0 [] = ErlangTuple [ErlangAtom "ok", H.make_string "."]  -- XD
 erlps__get_cwd__0 args = EXC.badarity (ErlangFun 0 erlps__get_cwd__0) args
+
+erlps__format_error__1 :: ErlangFun
+erlps__format_error__1 [ErlangTuple [_, ErlangAtom "file", ErlangAtom "undefined_script"]] =
+  H.make_string "no value returned from script"
+erlps__format_error__1 [ErlangTuple [line, ErlangAtom "file", ErlangTuple [clas, reason, stack]]] = H.make_string $ show line <> ": evaluation failed with reason " <> show clas <> ":" <> show reason <> " and stacktrace " <> show stack
+erlps__format_error__1 [ErlangTuple [line, ErlangAtom "file", ErlangTuple [reason, stack]]] = H.make_string $ show line <> ": evaluation failed with reason " <> show reason <> " and stacktrace " <> show stack
+erlps__format_error__1 [ErlangTuple [line, ErlangAtom modul, reason]] =
+  BIF.erlang__append__2 [H.make_string $ show line <> ": ", BIF.do_remote_fun_call modul "erlps__format_error__1" [reason]]
+erlps__format_error__1 [ErlangAtom "badarg"] = H.make_string "bad argument"
+erlps__format_error__1 [ErlangAtom "system_limit"] = H.make_string "a system limit was hit, probably not enough ports"
+erlps__format_error__1 [ErlangAtom "terminated"] = H.make_string "the file server process is terminated"
+erlps__format_error__1 [e] = H.make_string $ "some other error that I was too lazy to implement: " <> show e
+erlps__format_error__1 args = EXC.badarity (ErlangFun 0 erlps__format_error__1) args
